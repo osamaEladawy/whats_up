8bebd65f629b45a38c851ac9b506ab55e0f775b9
Osama Eladawy <classcar200@gmail.com>
2024-12-17 14:22:58 +0200

    update home

--- lib/shared/resources/images_resources.dart
+++ lib/shared/resources/images_resources.dart
@@ -1,1 +1,8 @@
-class ImagesResources {}
+class ImagesResources {
+  static const IMAGE_PATH = "assets/images";
+  static const bg_image = "$IMAGE_PATH/bg_image.png";
+  static const profile_default = "$IMAGE_PATH/profile_default.png";
+  static const meta = "$IMAGE_PATH/meta.png";
+  static const whats_app_logo = "$IMAGE_PATH/whats_app_logo.png";
+  static const whatsapp_bg_image = "$IMAGE_PATH/whatsapp_bg_image.png";
+}
